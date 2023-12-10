2a68de510582ce1c12db969f00476126a479dd56
Rhyan Araujo Chaves <rhyanaraujochaves@gmail.com>
2023-12-10 18:07:01 -0300

    Home Page finished

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,11 +1,26 @@
 import Image from 'next/image'
-import banner from '@/public/imgs/image-background.png'
 import styles from '@/app/sass/home.module.sass'
+import logo from '@/public/imgs/main.png'
+import collection from '@/public/imgs/collection.jpg'
+import Link from 'next/link'
+import adesivo from '../../public/imgs/restricted.png'
+import {} from 'react-icons'
 
 export default function Banner(){
     return(
         <div className={styles.home}>
-            <Image src={banner} alt='banner' className={styles.home_image} />
+            <Image src={logo} alt='logo' className={styles.home_image}/>
+            <div className={styles.promo}>
+                <Link href='/myproducts'>
+                <Image src={collection} alt='promo' className={styles.promo_image} />
+                <h2> Palace® New Drop </h2>
+                <h3> O verão chegou e com ele uma nova moda, veja agora o novo drop Palace da Angels</h3>
+                </Link>
+            </div>
+            <div className={styles.adesivo}>
+                <Image src={adesivo}  alt='restrito'/>
+
+            </div>
         </div>
     )
 }
